71c0dd10de182b764916e0eb788d6e9abdf22603
Tolly <34184902+TollyH@users.noreply.github.com>
2022-12-01 19:42:23 +0000

    D01 completed + runner program

---
+++ D01.cs
@@ -0,0 +1,44 @@
+﻿namespace AdventOfCode
+{
+    public static class D01
+    {
+        public static int PartOne(string[] input)
+        {
+            List<int> elves = new() { 0 };
+            int currentElf = 0;
+            foreach (string line in input)
+            {
+                if (line == "")
+                {
+                    currentElf++;
+                    elves.Add(0);
+                }
+                else
+                {
+                    elves[currentElf] += int.Parse(line);
+                }
+            }
+            return elves.Max();
+        }
+
+        public static int PartTwo(string[] input)
+        {
+            List<int> elves = new() { 0 };
+            int currentElf = 0;
+            foreach (string line in input)
+            {
+                if (line == "")
+                {
+                    currentElf++;
+                    elves.Add(0);
+                }
+                else
+                {
+                    elves[currentElf] += int.Parse(line);
+                }
+            }
+            elves.Sort((x, y) => y - x);
+            return elves.Take(3).Sum();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode
+{
+    public static class Program
+    {
+        public static readonly int DaysToRun = 1;
+
+        public static void Main(string[] args)
+        {
+            for (int day = 1; day <= DaysToRun; day++)
+            {
+                // Reflection is used to dynamically access each day's class, instead of specifying each one individually
+                Console.WriteLine($"==== Day {day:00} ====");
+                string[] input = File.ReadAllText($"input{day:00}.txt").Trim().Split('\n');
+                Console.WriteLine(Type.GetType($"AdventOfCode.D{day:00}")!.GetMethod("PartOne")!.Invoke(null, new object[] { input }));
+                Console.WriteLine(Type.GetType($"AdventOfCode.D{day:00}")!.GetMethod("PartTwo")!.Invoke(null, new object[] { input }));
+                Console.WriteLine();
+            }
+        }
+    }
+}
