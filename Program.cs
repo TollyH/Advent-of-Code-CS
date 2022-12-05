43d7de2d4e3a482a804e532c001c33bf936e77bb
Tolly <ptolemyhill@sky.com>
2022-12-05 09:51:48 +0000

    Improve launcher program

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
-﻿namespace AdventOfCode.Yr2022
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Yr2022
 {
     public static class Program
     {
-        public static readonly int DaysToRun = Directory.GetFiles(Environment.CurrentDirectory, "*??.txt").Length;
-
         public static void Main(string[] args)
         {
-            for (int day = 1; day <= DaysToRun; day++)
+            foreach (string filename in Directory.GetFiles(Environment.CurrentDirectory, "*??.txt"))
             {
+                int day = int.Parse(Regex.Match(filename, "input([0-9]{2}).txt").Groups[1].Value);
                 // Reflection is used to dynamically access each day's class, instead of specifying each one individually
                 Console.WriteLine($"==== Day {day:00} ====");
-                string[] input = File.ReadAllText($"input{day:00}.txt").Trim().Split('\n');
+                string[] input = File.ReadAllText(filename).TrimEnd().Split('\n');
                 Console.WriteLine(Type.GetType($"AdventOfCode.Yr2022.D{day:00}")!.GetMethod("PartOne")!.Invoke(null, new object[] { input }));
                 Console.WriteLine(Type.GetType($"AdventOfCode.Yr2022.D{day:00}")!.GetMethod("PartTwo")!.Invoke(null, new object[] { input }));
                 Console.WriteLine();
