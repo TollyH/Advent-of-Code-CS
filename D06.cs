c55a5145b3e0976bcfe0b44cdd289564c18148da
Tolly <ptolemyhill@sky.com>
2022-12-06 08:54:48 +0000

    Complete day 6

---
+++ D06.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode.Yr2022
+{
+    public static class D06
+    {
+        public static int PartOne(string[] input)
+        {
+            string data = input[0];
+            for (int i = 3; i < data.Length; i++)
+            {
+                string slice = data[(i - 3)..(i + 1)];
+                if (slice.Length == new HashSet<char>(slice).Count)
+                {
+                    return i + 1;
+                }
+            }
+            return -1;
+        }
+
+        public static int PartTwo(string[] input)
+        {
+            string data = input[0];
+            for (int i = 13; i < data.Length; i++)
+            {
+                string slice = data[(i - 13)..(i + 1)];
+                if (slice.Length == new HashSet<char>(slice).Count)
+                {
+                    return i + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
