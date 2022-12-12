b4326e6fe52a1f95fe32b1b0e44712b3c9a1038a
Tolly <34184902+TollyH@users.noreply.github.com>
2022-12-12 19:31:20 +0000

    D12: Optimisation and simplification
    As each point on the map is equally weighted, a simple BFS suffices over Dijkstra's

--- D12.cs
+++ D12.cs
@@ -4,18 +4,12 @@ namespace AdventOfCode.Yr2022
 {
     public static class D12
     {
-        private struct SquareData
-        {
-            public int Distance { get; set; }
-            public Point? Previous { get; set; }
-        }
-
         public static int PartOne(string[] input)
         {
             char[,] map = new char[input[0].Length, input.Length];
-            Dictionary<Point, SquareData> unvisited = new();
-            Dictionary<Point, SquareData> visited = new();
-            List<(int, Point)> queue = new();
+            Dictionary<Point, Point?> unvisited = new();
+            Dictionary<Point, Point?> visited = new();
+            Queue<Point> queue = new();
             Point start = new();
             Point end = new();
             for (int y = 0; y < input.Length; y++)
@@ -27,31 +21,27 @@ namespace AdventOfCode.Yr2022
                         start.X = x;
                         start.Y = y;
                         map[x, y] = 'a';
-                        unvisited[new Point(x, y)] = new() { Distance = 0 };
-                        queue.Add((0, new Point(x, y)));
+                        visited[new Point(x, y)] = null;
+                        queue.Enqueue(new Point(x, y));
                     }
                     else if (input[y][x] == 'E')
                     {
                         end.X = x;
                         end.Y = y;
                         map[x, y] = 'z';
-                        unvisited[new Point(x, y)] = new() { Distance = int.MaxValue };
+                        unvisited[new Point(x, y)] = null;
                     }
                     else
                     {
                         map[x, y] = input[y][x];
-                        unvisited[new Point(x, y)] = new() { Distance = int.MaxValue };
+                        unvisited[new Point(x, y)] = null;
                     }
                 }
             }
 
             while (unvisited.Count > 0)
             {
-                (int distance, Point coord) = queue[0];
-                SquareData currentData = unvisited[coord];
-                _ = unvisited.Remove(coord);
-                visited[coord] = currentData;
-                queue.RemoveAt(0);
+                Point coord = queue.Dequeue();
 
                 if (coord == end)
                 {
@@ -82,18 +72,13 @@ namespace AdventOfCode.Yr2022
                     {
                         continue;
                     }
-                    SquareData adjacent = unvisited[adj];
-                    int newDistance = distance + 1;
-                    if (newDistance < unvisited[adj].Distance && (map[adj.X, adj.Y] - map[coord.X, coord.Y] <= 1))
+                    if (map[adj.X, adj.Y] - map[coord.X, coord.Y] <= 1)
                     {
-                        adjacent.Distance = newDistance;
-                        adjacent.Previous = coord;
-                        unvisited[adj] = adjacent;
-                        queue.Add((newDistance, adj));
+                        visited[adj] = coord;
+                        _ = unvisited.Remove(adj);
+                        queue.Enqueue(adj);
                     }
                 }
-
-                queue.Sort((x, y) => x.Item1.CompareTo(y.Item1));
             }
 
             List<Point> finalPath = new();
@@ -101,7 +86,7 @@ namespace AdventOfCode.Yr2022
             while (current is not null)
             {
                 finalPath.Add(current.Value);
-                current = visited[current.Value].Previous;
+                current = visited[current.Value];
             }
             return finalPath.Count - 1;
         }
@@ -138,31 +123,33 @@ namespace AdventOfCode.Yr2022
 
             foreach (Point start in startPoints)
             {
-                Dictionary<Point, SquareData> unvisited = new();
-                Dictionary<Point, SquareData> visited = new();
-                List<(int, Point)> queue = new();
+                Dictionary<Point, Point?> unvisited = new();
+                Dictionary<Point, Point?> visited = new();
+                Queue<Point> queue = new();
                 for (int y = 0; y < input.Length; y++)
                 {
                     for (int x = 0; x < input[0].Length; x++)
                     {
-                        unvisited[new Point(x, y)] = new() { Distance = int.MaxValue };
+                        Point pt = new(x, y);
+                        if (pt == start)
+                        {
+                            visited[start] = null;
+                            queue.Enqueue(start);
+                        }
+                        else
+                        {
+                            unvisited[pt] = null;
+                        }
                     }
                 }
-                unvisited[start] = new() { Distance = 0 };
-                queue.Add((0, start));
                 bool found = true;
                 while (unvisited.Count > 0)
                 {
-                    if (!queue.Any())
+                    if (!queue.TryDequeue(out Point coord))
                     {
                         found = false;
                         break;
                     }
-                    (int distance, Point coord) = queue[0];
-                    SquareData currentData = unvisited[coord];
-                    _ = unvisited.Remove(coord);
-                    visited[coord] = currentData;
-                    queue.RemoveAt(0);
 
                     if (coord == end)
                     {
@@ -193,18 +180,13 @@ namespace AdventOfCode.Yr2022
                         {
                             continue;
                         }
-                        SquareData adjacent = unvisited[adj];
-                        int newDistance = distance + 1;
-                        if (newDistance < unvisited[adj].Distance && (map[adj.X, adj.Y] - map[coord.X, coord.Y] <= 1))
+                        if (map[adj.X, adj.Y] - map[coord.X, coord.Y] <= 1)
                         {
-                            adjacent.Distance = newDistance;
-                            adjacent.Previous = coord;
-                            unvisited[adj] = adjacent;
-                            queue.Add((newDistance, adj));
+                            visited[adj] = coord;
+                            _ = unvisited.Remove(adj);
+                            queue.Enqueue(adj);
                         }
                     }
-
-                    queue.Sort((x, y) => x.Item1.CompareTo(y.Item1));
                 }
                 if (!found)
                 {
@@ -216,7 +198,7 @@ namespace AdventOfCode.Yr2022
                 while (current is not null)
                 {
                     finalPath.Add(current.Value);
-                    current = visited[current.Value].Previous;
+                    current = visited[current.Value];
                 }
                 if (finalPath.Count - 1 < minPathLength)
                 {
