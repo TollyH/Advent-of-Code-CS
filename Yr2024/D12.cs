2df8a1f06c9cdb3551eacd9e313730bacbc2723f
TollyH <ptolemyhill@sky.com>
2024-12-13 19:34:15 +0000

    D12 + D13 P1

---
+++ Yr2024/D12.cs
@@ -0,0 +1,76 @@
+﻿using System.Drawing;
+
+namespace AdventOfCode.Yr2024
+{
+    public static class D12
+    {
+        private static readonly Point[] allowedMoves = new Point[4]
+        {
+            new(-1, 0),
+            new(1, 0),
+            new(0, -1),
+            new(0, 1)
+        };
+
+        private readonly struct Region(int perimeter, int area)
+        {
+            public readonly int Perimeter = perimeter;
+            public readonly int Area = area;
+        }
+
+        public static int PartOne(string[] input)
+        {
+            HashSet<Point> searchedPoints = new();
+            List<Region> regions = new();
+
+            for (int y = 0; y < input.Length; y++)
+            {
+                string line = input[y];
+                for (int x = 0; x < line.Length; x++)
+                {
+                    Point pnt = new(x, y);
+
+                    int perimeter = 0;
+                    int area = 0;
+
+                    Queue<Point> searchQueue = new();
+                    searchQueue.Enqueue(pnt);
+
+                    while (searchQueue.TryDequeue(out pnt))
+                    {
+                        if (!searchedPoints.Add(pnt))
+                        {
+                            continue;
+                        }
+
+                        area++;
+
+                        foreach (Point move in allowedMoves)
+                        {
+                            Point newPoint = new(pnt.X + move.X, pnt.Y + move.Y);
+                            if (newPoint.X >= 0 && newPoint.Y >= 0
+                                && newPoint.X < input.Length && newPoint.Y < input[0].Length
+                                && input[newPoint.Y][newPoint.X] == input[pnt.Y][pnt.X])
+                            {
+                                searchQueue.Enqueue(newPoint);
+                            }
+                            else
+                            {
+                                perimeter++;
+                            }
+                        }
+                    }
+
+                    regions.Add(new Region(perimeter, area));
+                }
+            }
+
+            return regions.Sum(r => r.Perimeter * r.Area);
+        }
+
+        public static int PartTwo(string[] input)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Yr2024/D13.cs
@@ -0,0 +1,68 @@
+﻿using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Yr2024
+{
+    public static partial class D13
+    {
+        private readonly record struct Machine(Point AButton, Point BButton, Point Prize);
+
+        [GeneratedRegex("([0-9]+).*?([0-9]+)")]
+        public static partial Regex TwoNumberRegex();
+
+        public static int PartOne(string[] input)
+        {
+            List<Machine> machines = new();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                Match lineOne = TwoNumberRegex().Match(input[i++]);
+                Match lineTwo = TwoNumberRegex().Match(input[i++]);
+                Match lineThree = TwoNumberRegex().Match(input[i++]);
+
+                machines.Add(new Machine(
+                    new Point(int.Parse(lineOne.Groups[1].Value), int.Parse(lineOne.Groups[2].Value)),
+                    new Point(int.Parse(lineTwo.Groups[1].Value), int.Parse(lineTwo.Groups[2].Value)),
+                    new Point(int.Parse(lineThree.Groups[1].Value), int.Parse(lineThree.Groups[2].Value))));
+            }
+
+            int total = 0;
+
+            foreach (Machine machine in machines)
+            {
+                for (int i = 0; i <= 200; i++)
+                {
+                    bool found = false;
+                    int tokens = 0;
+                    for (int aPresses = 0; aPresses <= i && aPresses <= 100 && !found; aPresses++)
+                    {
+                        int bPresses = i - aPresses;
+                        if (bPresses > 100)
+                        {
+                            continue;
+                        }
+                        Point target = new(
+                            (machine.AButton.X * aPresses) + (machine.BButton.X * bPresses),
+                            (machine.AButton.Y * aPresses) + (machine.BButton.Y * bPresses));
+                        if (target == machine.Prize)
+                        {
+                            tokens = aPresses * 3 + bPresses;
+                            found = true;
+                        }
+                    }
+                    if (found)
+                    {
+                        total += tokens;
+                    }
+                }
+            }
+
+            return total;
+        }
+
+        public static int PartTwo(string[] input)
+        {
+            return 0;
+        }
+    }
+}
